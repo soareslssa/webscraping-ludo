9e0639958636c0b9fa945f3cd84f18ca001096c1
soareslssa <soareslssa.dev@gmail.com>
2024-01-30 00:43:51 -0300

    feat: implementa iteracao de links ludopedia

--- editoraws.py
+++ editoraws.py
@@ -3,33 +3,47 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import re
 
-url = "https://ludopedia.com.br/jogo/dungeons-dragons-5th-edition-player-s-handbook"
+# Crie uma lista para armazenar as linhas do DataFrame
+data = []
 
-response = requests.get(url)
+arquivo = pd.read_csv('boardgames.csv', sep=';', header=0, nrows=10)
 
-content = response.content
-site = BeautifulSoup(content, 'html.parser')
+contador = 0
+for linha in arquivo.itertuples():
 
-bloco_descricao = site.find('div', class_='mar-btm bg-gray-light pad-all')
-tag_a = bloco_descricao.find('a')
+    url = linha.link_ludopedia
 
-href = tag_a.get('href')
-id_match = re.search(r'/editora/(\d+)/', href)
+    response = requests.get(url)
+    content = response.content
+    site = BeautifulSoup(content, 'html.parser')
 
-editora_id = id_match.group(1)
-editora_name = tag_a.text
-editora_link = tag_a.get('href')
+    bloco_descricao = site.find('div', class_='mar-btm bg-gray-light pad-all')
+    tag_a = bloco_descricao.find('a')
 
-descrica_body = site.find('div', id='bloco-descricao-sm')
-p_descricao = descrica_body.find('p')
+    href = tag_a.get('href')
+    
+    # Verificar se há uma correspondência antes de tentar acessar grupos
+    id_match = re.search(r'/editora/(\d+)/', href)
+    editora_id = id_match.group(1) if id_match else None
 
-descricao = p_descricao.text
+    editora_name = tag_a.text
+    editora_link = tag_a.get('href')
 
-# dataframe
+    descrica_body = site.find('div', id='bloco-descricao-sm')
+    p_descricao = descrica_body.find('p')
 
-df = pd.DataFrame({'id': [editora_id], 'name': [editora_name], 'link': [editora_link], 'descricao': [descricao]})
+    descricao = p_descricao.text
 
+    # Adicionar dados à lista
+    data.append({'id_ludopedia': linha.id_ludopedia,
+                 'name': linha.name,
+                 'id_editora': editora_id,
+                 'editora_name': editora_name,
+                 'editora_link': editora_link,
+                 'description': descricao})
 
-# exportação
+# Criar DataFrame a partir da lista
+df = pd.DataFrame(data)
+
+# Exportar DataFrame para CSV
 df.to_csv('editoras.csv', index=False)
-
